76dc230f2e4ca32e36eb388dab8d9af3bb72051b
wanjun <wanjunsun8@163.com>
2020-03-24 10:42:49 +0800

    :sparkles: 新增几种button 样式

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <qk-button type="default">默认</qk-button>
+    <qk-button>默认</qk-button>
     <qk-button type="primary">primary</qk-button>
     <qk-button type="success">成功</qk-button>
     <qk-button type="warning">警告</qk-button>
