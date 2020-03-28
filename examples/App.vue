793eb39bc2ecdf31eaa2d5fd3733265d21362c6a
wanjun <wanjunsun8@163.com>
2020-03-28 14:06:57 +0800

    :art: button-group 样式优化

--- examples/App.vue
+++ examples/App.vue
@@ -9,11 +9,12 @@
     <qk-button type="text">link</qk-button>
     <qk-button type="primary" size="small">小</qk-button>
     <qk-button type="primary" size="large">大</qk-button>
-    <qk-button size="large" shape="circle">圆</qk-button>
+    <qk-button size="large" shape="circle">圆角</qk-button>
     <qk-button long>长</qk-button>
     <qk-button-group shape="circle" size="large">
-      <qk-button>成功</qk-button><qk-button>成功</qk-button><qk-button>成功</qk-button><qk-button>成功</qk-button>
-      <qk-button>成功</qk-button>
+      <qk-button type="success">成功</qk-button><qk-button>成功</qk-button
+      ><qk-button type="warning">成功</qk-button><qk-button>成功</qk-button>
+      <qk-button type="error">成功</qk-button>
     </qk-button-group>
   </div>
 </template>
